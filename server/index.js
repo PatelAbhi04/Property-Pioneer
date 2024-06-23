1353e6357b7cc122daee1301dd5f0d3c14315096
PatelAbhi04 <abhinpatel11@gmail.com>
2024-06-23 18:10:24 +0530

    resdy to host

--- server/index.js
+++ server/index.js
@@ -25,7 +25,6 @@ const __dirname = path.resolve();
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/listing",listingRouter);
-
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
